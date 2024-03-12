26994e047628e4bcd9591b2ec5d8da286b0bd05f
Matheus Passos <40068795+profmatheuspassos@users.noreply.github.com>
2024-03-12 07:39:11 +0200

    Create ppt.py

---
+++ ppt.py
@@ -0,0 +1,48 @@
+import os
+import time
+
+pontosVoce = 0
+pontosComp = 0
+numJogada = [1, 2, 3]
+nomeJogada = ["Papel", "Pedra", "Tesoura"]
+
+def cabecalho():
+    os.system("cls" if os.name == "nt" else "clear")
+    print("==============================================")
+    print("Bem-vindo(a) ao jogo \"Pedra, Papel ou Tesoura\"")
+    print("==============================================")
+    print("\n")
+
+def placar():
+    print("PLACAR:")
+    print(f"Você: {pontosVoce}")
+    print(f"Você: {pontosComp}")
+    print("\n")
+
+def opcoes():
+    print("Opções: 1 - Papel | 2 - Pedra | 3 - Tesoura")
+
+def jogada(opcaoVoce, opcaoComp):
+    print("==================")
+    print(f"Sua jogada: {opcaoVoce}")
+    print(f"Jogada do computador: {opcaoComp}")
+    # FAZER A CONTA - lembrar que pode ter empate
+    # indicar quem venceu
+    print("==================")
+
+while True:
+    cabecalho()
+    placar()
+    opcoes()
+    try:
+        lance = int(input("Escolha seu lance: "))
+        # CHAMAR FUNÇÃO JOGADA COM DOIS PARÂMETROS (jogadaVoce e jogadaComp)
+        # ATUALIZAR PLACAR
+        # CHAMAR MENU DE JOGAR NOVAMENTE - Sim ou Não
+        print(lance)
+        break
+    except:
+        print("Insira apenas o número da opção (1, 2 ou 3). Tente novamente.")
+        print("Voltando ao menu inicial...")
+        time.sleep(3)
+        continue
