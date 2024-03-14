29ab1c604856c39915863e94a3dbb1af39a28512
Matheus Passos <40068795+profmatheuspassos@users.noreply.github.com>
2024-03-14 07:10:29 +0200

    Update ppt.py

--- ppt.py
+++ ppt.py
@@ -17,8 +17,22 @@ def cabecalho():
 def placar():
     print("PLACAR:")
     print(f"Você: {pontosVoce}")
-    print(f"Você: {pontosComp}")
+    print(f"Computador: {pontosComp}")
     print("\n")
+
+def placarFinal():
+    print("PLACAR FINAL:")
+    print(f"Você: {pontosVoce}")
+    print(f"Computador: {pontosComp}")
+    print("\n")
+    print("Até a próxima!")
+    time.sleep(3)
+    os.system("cls" if os.name == "nt" else "clear")
+
+def erroOpcoes():
+    print("Insira apenas o número 1 ou o número 2.")
+    print("Voltando ao menu inicial...")
+    time.sleep(3)
 
 def opcoes():
     print("Opções: 1 - Papel | 2 - Pedra | 3 - Tesoura")
@@ -27,6 +41,16 @@ def jogada(opcaoVoce, opcaoComp):
     print("==================")
     print(f"Sua jogada: {opcaoVoce}")
     print(f"Jogada do computador: {opcaoComp}")
+    if opcaoVoce > opcaoComp:
+        print("Você venceu!")
+        pontosVoce += 1
+    elif opcaoVoce < opcaoComp:
+        print("O computador venceu!")
+        pontosComp += 1
+    else:
+        print("Empate!")
+        pontosComp += 1
+        pontosVoce += 1
     # FAZER A CONTA - lembrar que pode ter empate
     # indicar quem venceu
     print("==================")
@@ -36,14 +60,28 @@ while True:
     placar()
     opcoes()
     try:
-        lanceVoce = int(input("Escolha seu lance: "))
+        lanceVoce = int(input("\nEscolha seu lance: "))
         lanceComp = random.randint(1,3)
-        print(lanceComp)
+        jogada(lanceVoce, lanceComp)
+        try:
+            sair = int(input("Você quer sair ou jogar novamente? 1 - SAIR | 2 - JOGAR NOVAMENTE"))
+            if sair == 1:
+                placarFinal()
+                break
+            elif sair == 2:
+                print("Voltando ao menu inicial...")
+                time.sleep(3)
+                continue
+            else:
+                erroOpcoes()
+                continue
+        except:
+            erroOpcoes()
+            continue
         # CHAMAR FUNÇÃO JOGADA COM DOIS PARÂMETROS (jogadaVoce e jogadaComp)
         # ATUALIZAR PLACAR
         # CHAMAR MENU DE JOGAR NOVAMENTE - Sim ou Não
         # Se sair mostra o placar final
-        print(lanceVoce)
         break
     except:
         print("Insira apenas o número da opção (1, 2 ou 3). Tente novamente.")
